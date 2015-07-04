5bde86accb4e328bcfbf2f7bfd8e4eb0c22e2959
Uriel Corfa <uriel@corfa.fr>
2015-07-04 14:25:56 -0700

    Add LabelGauge and CallbackLabelGauge.
    These two util classes are basically label-valued gauges whose entire
    value set can be provided all at once (either by a set method or via a
    callback). I will do a PR to offer this in prometheus_client once I'm
    satisfied with how this works.

--- django_prometheus/utils.py
+++ django_prometheus/utils.py
@@ -1,6 +1,7 @@
+from prometheus_client import REGISTRY, Metric
+import re
+from threading import Lock
 import time
-
-# TODO(korfuri): if python>3.3, use perf_counter() or monotonic().
 
 
 def Time():
@@ -11,6 +12,7 @@ def Time():
     treated as an opaque object. It can be compared to the current
     time with TimeSince().
     """
+    # TODO(korfuri): if python>3.3, use perf_counter() or monotonic().
     return time.time()
 
 
@@ -29,3 +31,76 @@ def PowersOf(logbase, count, lower=0, include_zero=True):
         return [logbase ** i for i in range(lower, count+lower)]
     else:
         return [0] + [logbase ** i for i in range(lower, count+lower)]
+
+
+# This should be taken directly from prometheus_client
+_METRIC_NAME_RE = re.compile(r'^[a-zA-Z_:][a-zA-Z0-9_:]*$')
+_METRIC_LABEL_NAME_RE = re.compile(r'^[a-zA-Z_:][a-zA-Z0-9_:]*$')
+
+
+class BaseLabelGauge(object):
+    """A base class for LabelGauge and CallbackLabelGauge.
+
+    This provides the basic functionality for a metric of type "A
+    label-valued gauge whose values are all updated at the same time".
+    """
+    def __init__(self, name, documentation, labelnames, namespace='',
+                 subsystem='', registry=REGISTRY):
+        # This should be deduplicated against _MetricWrapper.init
+        full_name = ''
+        if namespace:
+            full_name += namespace + '_'
+        if subsystem:
+            full_name += subsystem + '_'
+        full_name += name
+        if not _METRIC_NAME_RE.match(full_name):
+            raise ValueError('Invalid metric name: ' + full_name)
+
+        if registry:
+            registry.register(self)
+
+        def collect():
+            metric = Metric(full_name, documentation, 'gauge')
+            for suffix, labels, value in self.get_samples():
+                metric.add_sample(full_name + suffix, labels, value)
+            return [metric]
+        self.collect = collect
+
+    def get_samples(self):
+        with self._lock:
+            samples = self._samples
+        return samples
+
+    def set_samples(self, samples):
+        with self._lock:
+            self._samples = samples
+
+
+class LabelGauge(BaseLabelGauge):
+    """A label-valued gauge whose values can be updated atomically."""
+    def __init__(self, *args, **kwargs):
+        self._samples = []
+        self._lock = Lock()
+        super(LabelGauge, self).__init__(*args, **kwargs)
+
+    def get_samples(self):
+        with self._lock:
+            samples = self._samples
+        return samples
+
+    def set_samples(self, samples):
+        with self._lock:
+            self._samples = samples
+
+
+class CallbackLabelGauge(BaseLabelGauge):
+    """A label-valued gauge whose values are all provided at once by a
+    callback.
+    """
+    def __init__(self, name, documentation, labelnames, callback, **kwargs):
+        self._callback = callback
+        super(CallbackLabelGauge, self).__init__(
+            name, documentation, labelnames, **kwargs)
+
+    def get_samples(self):
+        return self._callback()
--- tests/test_django_prometheus.py
+++ tests/test_django_prometheus.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from django_prometheus.utils import PowersOf
+from django_prometheus.utils import PowersOf, LabelGauge, CallbackLabelGauge
 import unittest
 
 
@@ -18,6 +18,35 @@ class DjangoPrometheusTest(unittest.TestCase):
         self.assertEqual(
             [4, 8, 16, 32, 64, 128],
             PowersOf(2, 6, lower=2, include_zero=False))
+
+
+class LabelGaugeTest(unittest.TestCase):
+    def testSetGet(self):
+        lg = LabelGauge('test', 'test LabelGauge', ['x', 'y'])
+        samples = [
+            (u'', {'x': 'a', 'y': 'a'}, 1.0),
+            (u'', {'x': 'a', 'y': 'b'}, 2.0),
+        ]
+        lg.set_samples(samples)
+        self.assertEqual(samples, lg.get_samples())
+        samples2 = [
+            (u'', {'x': 'b', 'y': 'a'}, 3.0),
+            (u'', {'x': 'a', 'y': 'b'}, 4.0),
+        ]
+        lg.set_samples(samples2)
+        self.assertEqual(samples2, lg.get_samples())
+
+
+class CallbackLabelGaugeTest(unittest.TestCase):
+    def testCallback(self):
+        samples = [
+            (u'', {'x': 'a', 'y': 'a'}, 1.0),
+            (u'', {'x': 'a', 'y': 'b'}, 2.0),
+        ]
+
+        clg = CallbackLabelGauge('test', 'test CallbackLabelGauge',
+                                 ['x', 'y'], lambda: samples)
+        self.assertEqual(samples, clg.get_samples())
 
 
 if __name__ == 'main':
