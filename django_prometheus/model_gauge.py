7fd8e4fb21414cd710332ee6c49082a05164be00
Uriel Corfa <uriel@corfa.fr>
2015-07-04 15:03:52 -0700

    Add support for a non-label-valued QuerySetGauge.
    Rename QuerySetGauge to QuerySetLabelGauge to clarify the difference.

--- django_prometheus/model_gauge.py
+++ django_prometheus/model_gauge.py
@@ -1,8 +1,9 @@
 from django_prometheus.utils import CallbackLabelGauge
 from django.db.models import Count
+from prometheus_client import Gauge
 
 
-class QuerySetGauge(object):
+class QuerySetLabelGauge(object):
     """A gauge that exports the results of a queryset.
 
     Examples:
@@ -10,9 +11,9 @@ class QuerySetGauge(object):
     Assuming we have a `Book` model with fields `published` and `author`:
       ```
       qs = Book.objects.filter(published=True)
-      g = QuerySetGauge(qs, 'books_by_author_total',
-                        'Count of books by author.',
-                        ['author'])
+      g = QuerySetLabelGauge(qs, 'books_by_author_total',
+                             'Count of books by author.',
+                             ['author'])
       ```
     Will export:
       ```
@@ -22,10 +23,7 @@ class QuerySetGauge(object):
     """
 
     def __init__(self, queryset, name, description, labels, count_by='pk'):
-        if labels:
-            self._queryset = queryset.values(*labels)
-        else:
-            self._queryset = queryset
+        self._queryset = queryset.values(*labels)
         self._g = CallbackLabelGauge(name, description, labels, self.Run)
         self._count_by = count_by
 
@@ -36,3 +34,19 @@ class QuerySetGauge(object):
             value = row.pop('total')
             samples.append((u'', row, value))
         return samples
+
+
+class QuerySetGauge(object):
+    """A gauge that exports the count of results in a queryset.
+
+    Examples:
+    ```
+      qs = Books.objects.filter(published=False)
+      g = QuerySetGauge(qs, 'books_unpublished_total',
+                        'Count of books not yet published.')
+    ```
+    Will export the number of unpublished books.
+    """
+    def __init__(self, queryset, name, description):
+        self._g = Gauge(name, description)
+        self._g.set_function(lambda: queryset.count())
--- tests/end2end/testapp/test_querysetgauge.py
+++ tests/end2end/testapp/test_querysetgauge.py
@@ -1,11 +1,11 @@
-from django_prometheus.model_gauge import QuerySetGauge
+from django_prometheus.model_gauge import QuerySetLabelGauge, QuerySetGauge
 from django_prometheus.testutils import PrometheusTestCaseMixin
 from django.test import TestCase
 from testapp.models import Ingredient, Recipe
 
 
-class TestQuerySetGauge(PrometheusTestCaseMixin, TestCase):
-    """Test QuerySetGauge."""
+class TestQuerySetGauges(PrometheusTestCaseMixin, TestCase):
+    """Test QuerySetGauge and QuerySetLabelGauge."""
 
     def setUp(self):
         spaghetti = Ingredient(name='spaghetti')
@@ -28,19 +28,23 @@ class TestQuerySetGauge(PrometheusTestCaseMixin, TestCase):
 
     def test_querysetgauge(self):
         qs = Recipe.objects
-        QuerySetGauge(qs, 'recipe_by_country_total',
-                      'Number of recipes by country', ['country'])
+        QuerySetLabelGauge(qs, 'recipe_by_country_total',
+                           'Number of recipes by country', ['country'])
 
         self.assertMetricEquals(
             2, 'recipe_by_country_total', country='FR')
         self.assertMetricEquals(
             1, 'recipe_by_country_total', country='IT')
 
+        Recipe(name='kouign amann', country='FR').save()
+        self.assertMetricEquals(
+            3, 'recipe_by_country_total', country='FR')
+
     def test_related_model(self):
         qs = Recipe.objects
-        QuerySetGauge(qs, 'ingredients_by_recipe_total',
-                      'Number of ingredients by recipe', ['name'],
-                      count_by='ingredients')
+        QuerySetLabelGauge(qs, 'ingredients_by_recipe_total',
+                           'Number of ingredients by recipe', ['name'],
+                           count_by='ingredients')
 
         self.assertMetricEquals(
             3, 'ingredients_by_recipe_total', name='bolognese')
@@ -48,12 +52,15 @@ class TestQuerySetGauge(PrometheusTestCaseMixin, TestCase):
             1, 'ingredients_by_recipe_total', name='cote de boeuf')
         self.assertMetricEquals(
             2, 'ingredients_by_recipe_total', name='boeuf carrottes')
+        Recipe(name='kouign amann', country='FR').save()
+        self.assertMetricEquals(
+            0, 'ingredients_by_recipe_total', name='kouign amann')
 
     def test_multiple_labels(self):
         qs = Recipe.objects
-        QuerySetGauge(qs, 'ingredients_by_recipe_country_total',
-                      'Number of ingredients by recipe and country',
-                      ['name', 'country'], count_by='ingredients')
+        QuerySetLabelGauge(qs, 'ingredients_by_recipe_country_total',
+                           'Number of ingredients by recipe and country',
+                           ['name', 'country'], count_by='ingredients')
 
         self.assertMetricEquals(3, 'ingredients_by_recipe_country_total',
                                 name='bolognese', country='IT')
@@ -61,3 +68,17 @@ class TestQuerySetGauge(PrometheusTestCaseMixin, TestCase):
                                 name='cote de boeuf', country='FR')
         self.assertMetricEquals(2, 'ingredients_by_recipe_country_total',
                                 name='boeuf carrottes', country='FR')
+
+        Recipe(name='kouign amann', country='FR').save()
+        self.assertMetricEquals(
+            0, 'ingredients_by_recipe_country_total',
+            name='kouign amann', country='FR')
+
+    def test_no_labels(self):
+        qs = Ingredient.objects
+        QuerySetGauge(qs, 'ingredients_total',
+                      'Total number of ingredients')
+
+        self.assertMetricEquals(4, 'ingredients_total')
+        Ingredient(name='rice').save()
+        self.assertMetricEquals(5, 'ingredients_total')
